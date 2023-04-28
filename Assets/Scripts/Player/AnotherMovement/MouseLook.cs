0f691e78aaf2fb714296f8b541a87e161222111d
JonyBr <Jelatinbumbir252@gmail.com>
2023-04-28 19:22:08 +0300

    Granade

--- Assets/Scripts/Player/AnotherMovement/MouseLook.cs
+++ Assets/Scripts/Player/AnotherMovement/MouseLook.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class MouseLook : MonoBehaviour
 {
@@ -10,25 +12,41 @@ public class MouseLook : MonoBehaviour
     [SerializeField] private float _sensitivity = 100f ;
     [SerializeField] private Transform _playerBody;
     [SerializeField] private float _xRotation = 0f;
+    [SerializeField] private Camera _camera;
 
+
+ 
     // Start is called before the first frame update
     void Start()
     {
 
-        Cursor.lockState = CursorLockMode.Locked;
+        // Cursor.lockState = CursorLockMode.Locked;
 
     }
-     
+
     // Update is called once per frame
     void Update()
     {
-        _mouseX = Input.GetAxis("Mouse X") * _sensitivity * Time.deltaTime;
-        _mouseY = Input.GetAxis("Mouse Y") * _sensitivity * Time.deltaTime;
+        if (Input.GetMouseButton(0))
+        {
+            _mouseX = Input.GetAxis("Mouse X") * _sensitivity * Time.deltaTime;
+            _mouseY = Input.GetAxis("Mouse Y") * _sensitivity * Time.deltaTime;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            _mouseX = 0f;
+            _mouseY = 0f;
+        }
 
         _xRotation -= _mouseY;
         _xRotation = Mathf.Clamp(_xRotation, -90f, 90f);
+        _camera.transform.localRotation = Quaternion.Euler(_xRotation, 0f, 0f);
 
-        transform.localRotation = Quaternion.Euler(_xRotation, 0f, 0f);
+
+
+
+
+
 
         _playerBody.Rotate(Vector3.up * _mouseX);
 
--- Assets/Scripts/Player/Weapon/Granade.cs
+++ Assets/Scripts/Player/Weapon/Granade.cs
@@ -5,23 +5,50 @@ using UnityEngine;
 public class Granade : MonoBehaviour
 {
     [SerializeField] private GameObject _granadePrefab;
+    [SerializeField] private GameObject _explosionPrefab;
+    [SerializeField] private Transform _positionToTrow;
     [SerializeField] private float _explosionForce;
+    [SerializeField] private float _UpwardForce;
     [SerializeField] private float _explosionSpeed;
-    public float _delay = 3f;
-    public float _radius = 20f;
+    public float delay = 3f;
+    public float radius = 20f;
+    public int quantityGranade = 3;
+    private bool _enabledToTrow = true;
+    [SerializeField] private Camera _cam ;
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
+        InputHandler();
+    }
+    private void InputHandler()
+    {
+        if(Input.GetKeyDown(KeyCode.G) && quantityGranade > 0) 
+        {
+            StartCoroutine(GrandeHendle());
+        }
+    }
+
+    public IEnumerator GrandeHendle()
+    {
+        quantityGranade--;
+        GameObject projectile = Instantiate(_granadePrefab, _positionToTrow.transform.position, _positionToTrow.transform.rotation);
+        
+        Rigidbody projecrileRB = projectile.GetComponent<Rigidbody>();
+
+        Vector3 forceToAdd = _cam.transform.forward * _explosionForce + gameObject.transform.up * _UpwardForce;
+
+        projecrileRB.AddForce(forceToAdd, ForceMode.Impulse);
+        yield return new WaitForSeconds(delay);
+        GameObject explotion = Instantiate(_explosionPrefab, projectile.transform.position, projectile.transform.rotation);
+        Destroy(projectile);
+        yield return new WaitForSeconds(delay);
+        Destroy(explotion);
         
     }
-    public void GrandeHendle()
-    {
 
-    }
 }
